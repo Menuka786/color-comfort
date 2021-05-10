b78f50ccb2447006f592d71f6d76e0141b020b04
Menuka Gusain <menukagusain@gmail.com>
2021-05-09 21:45:33 -0400

    added home screen folder

---
+++ frontend/src/products.js
@@ -0,0 +1,108 @@
+const proudcts =[
+
+{
+    _id:'1',
+    name: 'Soft Jaipuri bedsheet',
+    image: '',
+
+description:
+'soft and comfortable cotton bedsheet',
+
+brand: 'color and comfort',
+
+category: 'cottonsheets',
+price:35.99,
+conuntInStock:10,
+rating:4.5,
+numReviews:4,
+
+},
+{
+    _id:'2',
+    name: 'Soft Jaipuri bedsheet',
+    image: '',
+
+description:
+'soft and comfortable cotton bedsheet',
+
+brand: 'color and comfort',
+
+category: 'cottonsheets',
+price:35.99,
+conuntInStock:10,
+rating:4.5,
+numReviews:4,
+
+},
+{
+    _id:'3',
+    name: 'Soft Jaipuri bedsheet',
+    image: '',
+
+description:
+'soft and comfortable cotton bedsheet',
+
+brand: 'color and comfort',
+
+category: 'cottonsheets',
+price:35.99,
+conuntInStock:10,
+rating:4.5,
+numReviews:4,
+
+},
+{
+    _id:'4',
+    name: 'Soft Jaipuri bedsheet',
+    image: '',
+
+description:
+'soft and comfortable cotton bedsheet',
+
+brand: 'color and comfort',
+
+category: 'cottonsheets',
+price:35.99,
+conuntInStock:10,
+rating:4.5,
+numReviews:4,
+
+},
+{
+    _id:'5',
+    name: 'Soft Jaipuri bedsheet',
+    image: '',
+
+description:
+'soft and comfortable cotton bedsheet',
+
+brand: 'color and comfort',
+
+category: 'cottonsheets',
+price:35.99,
+conuntInStock:10,
+rating:4.5,
+numReviews:4,
+
+},
+{
+    _id:'6',
+    name: 'Soft Jaipuri bedsheet',
+    image: '',
+
+description:
+'soft and comfortable cotton bedsheet',
+
+brand: 'color and comfort',
+
+category: 'cottonsheets',
+price:35.99,
+conuntInStock:10,
+rating:4.5,
+numReviews:4,
+
+},
+
+
+
+]
